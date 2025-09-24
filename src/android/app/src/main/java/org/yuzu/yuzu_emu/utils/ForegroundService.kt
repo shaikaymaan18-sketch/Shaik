5ef6ecf03e769c7168f6bfc68a495a4e96c3c9d9
nyx <contact@innix.space>
2025-09-24 09:48:21 +0200

    license header

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/utils/ForegroundService.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/utils/ForegroundService.kt
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // Copyright 2023 Citra Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
