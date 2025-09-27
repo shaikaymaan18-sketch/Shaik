c936c1e8d44570c0c5b90d2a4441123fc983a04a
nyx-ynx <contact@nyxynx.dev>
2025-09-27 17:50:27 +0200

    typo

--- src/android/app/src/main/jni/android_settings.h
+++ src/android/app/src/main/jni/android_settings.h
@@ -80,7 +80,7 @@ namespace AndroidSettings {
                                                          Settings::Specialization::Paired, true,
                                                          true};
 
-        Settings::Setting<u32> perf_overlay_border{linkage, 0, "input_overlay_auto_hide",
+        Settings::Setting<u32> input_overlay_auto_hide{linkage, 0, "input_overlay_auto_hide",
                                                     Settings::Category::Overlay,
                                                     Settings::Specialization::Default, true, true,};
         Settings::Setting<bool> perf_overlay_background{linkage, false, "perf_overlay_background",
