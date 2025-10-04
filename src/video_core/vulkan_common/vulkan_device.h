6437467c180ddb243baa22a285008e88dae83ebe
Ribbit <ribbit@placeholder.com>
2025-10-03 20:26:23 -0700

    [vk] Disable broken EXTs on stock Adreno drivers.

--- src/video_core/vulkan_common/vulkan_device.h
+++ src/video_core/vulkan_common/vulkan_device.h
@@ -377,6 +377,10 @@ public:
 
     /// Returns true if shader int64 is supported.
     bool IsShaderInt64Supported() const {
+        const auto driver = GetDriverID();
+        if (driver == VK_DRIVER_ID_QUALCOMM_PROPRIETARY) {
+            return false;
+        }
         return features.features.shaderInt64;
     }
 
@@ -591,6 +595,10 @@ public:
 
     /// Returns true if the device supports VK_KHR_shader_atomic_int64.
     bool IsExtShaderAtomicInt64Supported() const {
+        const auto driver = GetDriverID();
+        if (driver == VK_DRIVER_ID_QUALCOMM_PROPRIETARY) {
+            return false;
+        }
         return extensions.shader_atomic_int64;
     }
 
