bc48ceef3b1e5ac7f2737246222112a3500ebc07
crueter <crueter@eden-emu.dev>
2025-10-14 20:12:18 -0400

    [meta] fix some compilation errors
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- src/video_core/renderer_vulkan/vk_buffer_cache.cpp
+++ src/video_core/renderer_vulkan/vk_buffer_cache.cpp
@@ -360,8 +360,7 @@ void BufferCacheRuntime::FreeDeferredStagingBuffer(StagingBufferRef& ref) {
     staging_pool.FreeDeferred(ref);
 }
 
-void BufferCacheRuntime::UniformRing::Init(const Device& device,
-                                           MemoryAllocator& alloc,
+void BufferCacheRuntime::UniformRing::Init(MemoryAllocator& alloc,
                                            u64 bytes, u32 alignment) {
     for (size_t i = 0; i < NUM_FRAMES; ++i) {
         VkBufferCreateInfo ci{
