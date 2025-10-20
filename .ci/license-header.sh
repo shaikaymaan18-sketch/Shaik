5feb4f140fe45095c417a156378d2544e660fcec
crueter <crueter@eden-emu.dev>
2025-10-19 21:02:53 -0400

    Fix license headers
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/license-header.sh
+++ .ci/license-header.sh
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # specify full path if dupes may exist
-EXCLUDE_FILES="CPM.cmake CPMUtil.cmake GetSCMRev.cmake sse2neon.h renderdoc_app.h tools/cpm externals/stb externals/glad externals/getopt externals/gamemode externals/FidelityFX-FSR externals/demangle externals/bc_decoder"
+EXCLUDE_FILES="CPM.cmake CPMUtil.cmake GetSCMRev.cmake sse2neon.h renderdoc_app.h tools/cpm tools/shellcheck.sh tools/update-cpm.sh externals/stb externals/glad externals/getopt externals/gamemode externals/FidelityFX-FSR externals/demangle externals/bc_decoder"
 
 # license header constants, please change when needed :))))
 YEAR=2025
@@ -96,6 +96,8 @@ check_header() {
 BASE=$(git merge-base master HEAD)
 FILES=$(git diff --name-only "$BASE")
 
+echo "$EXCLUDE_FILES"
+
 for file in $FILES; do
     [ -f "$file" ] || continue
 
@@ -104,7 +106,7 @@ for file in $FILES; do
 		case "$file" in
 			*"$pattern"*)
 				excluded=true
-				continue
+				break
 				;;
 			*)
 				excluded=false
