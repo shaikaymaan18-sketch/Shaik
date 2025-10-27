f7bc39ef01d7e41bb427728eeea428c9629e7011
kleidis <kleidis1@protonmail.com>
2025-10-27 17:26:28 +0100

    android: Rework setup fragment to use multiple buttons per-page
    Adapted from https://github.com/azahar-emu/azahar/commit/f771952e62a3c7c175a99c91635509b24a8300f6#diff-e59f69380a076aef2745f7ab65072ca25fc26c598e2ed177475a15fe44121b4d

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/fragments/AddGameFolderDialogFragment.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/fragments/AddGameFolderDialogFragment.kt
@@ -32,12 +32,14 @@ class AddGameFolderDialogFragment : DialogFragment() {
             .setTitle(R.string.add_game_folder)
             .setPositiveButton(android.R.string.ok) { _: DialogInterface, _: Int ->
                 val newGameDir = GameDir(folderUriString!!, binding.deepScanSwitch.isChecked)
-                homeViewModel.setGamesDirSelected(true)
                 val calledFromGameFragment = requireArguments().getBoolean(
                     "calledFromGameFragment",
                     false
                 )
-                gamesViewModel.addFolder(newGameDir, calledFromGameFragment)
+                val job = gamesViewModel.addFolder(newGameDir, calledFromGameFragment)
+                job.invokeOnCompletion {
+                    homeViewModel.setGamesDirSelected(true)
+                }
             }
             .setNegativeButton(android.R.string.cancel, null)
             .setView(binding.root)
