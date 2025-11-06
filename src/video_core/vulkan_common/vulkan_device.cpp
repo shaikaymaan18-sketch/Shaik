ee43e3bd78cc855e7ff8e5bd0ba0ea033c0960bb
DraVee <caiooliveirafarias0@gmail.com>
2025-11-06 02:18:49 +0100

    Aplicar correção

--- src/video_core/vulkan_common/vulkan_device.cpp
+++ src/video_core/vulkan_common/vulkan_device.cpp
@@ -744,7 +744,7 @@ Device::Device(VkInstance instance_, vk::PhysicalDevice physical_, VkSurfaceKHR 
                                VK_EXT_EXTENDED_DYNAMIC_STATE_2_EXTENSION_NAME);
     }
 
-    if (!extensions.extended_dynamic_state2 && extensions.extended_dynamic_state3 && !force_extensions) {
+    if (!extensions.extended_dynamic_state2 && extensions.extended_dynamic_state3) {
         LOG_INFO(Render_Vulkan,
                  "Removing extendedDynamicState3 due to missing extendedDynamicState2");
         RemoveExtensionFeature(extensions.extended_dynamic_state3, features.extended_dynamic_state3,
