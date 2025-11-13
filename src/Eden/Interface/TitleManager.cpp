1497d32cf687c4d26fc6d31e7600bf4ac92ae001
crueter <crueter@eden-emu.dev>
2025-11-13 02:22:36 -0500

    fix titlemanager
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- src/Eden/Interface/TitleManager.cpp
+++ src/Eden/Interface/TitleManager.cpp
@@ -11,19 +11,16 @@ const QString TitleManager::title() const
 {
     static const std::string description = std::string(Common::g_build_version);
     static const std::string build_id = std::string(Common::g_build_id);
-    static const std::string compiler = std::string(Common::g_compiler_id);
-
-    std::string yuzu_title;
-    if (Common::g_is_dev_build) {
-        yuzu_title = fmt::format("Eden Nightly | {}-{} | {}", description, build_id, compiler);
-    } else {
-        yuzu_title = fmt::format("Eden | {} | {}", description, compiler);
-    }
+    static const std::string yuzu_build = fmt::format("{} | {} | {}",
+                                                      std::string{Common::g_build_name},
+                                                      std::string{Common::g_build_version},
+                                                      std::string{Common::g_compiler_id}
+                                                      );
 
     const auto override_title = fmt::format(fmt::runtime(
                                                 std::string(Common::g_title_bar_format_idle)),
                                             build_id);
-    const auto window_title = override_title.empty() ? yuzu_title : override_title;
+    const auto window_title = override_title.empty() ? yuzu_build : override_title;
 
     // TODO(crueter): Running
 
