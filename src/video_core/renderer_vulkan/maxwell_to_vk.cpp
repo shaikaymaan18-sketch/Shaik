2ee220c5b56e8e5fa5fb08b702ccca56e83b7c7b
CamilleLaVey <camillelavey99@gmail.com>
2025-11-15 23:18:39 -0400

    Fix building/ licensing issues

--- src/video_core/renderer_vulkan/maxwell_to_vk.cpp
+++ src/video_core/renderer_vulkan/maxwell_to_vk.cpp
@@ -214,7 +214,6 @@ struct FormatTuple {
     {VK_FORMAT_ASTC_8x6_SRGB_BLOCK},                           // ASTC_2D_8X6_SRGB
     {VK_FORMAT_ASTC_6x5_UNORM_BLOCK},                          // ASTC_2D_6X5_UNORM
     {VK_FORMAT_ASTC_6x5_SRGB_BLOCK},                           // ASTC_2D_6X5_SRGB
-    {VK_FORMAT_B10G11R11_UFLOAT_PACK32},                      // B10G11R11_FLOAT
 
     // Depth formats
     {VK_FORMAT_D32_SFLOAT, Attachable},          // D32_FLOAT
