4f1096101b6d1160e0731b4ef820f14454b43a9c
Kleidis <kleidis1@protonmail.com>
2025-11-26 07:25:13 +0100

    license header

--- src/core/GameSettings.cpp
+++ src/core/GameSettings.cpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Eden Emulator Project
+// SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "core/GameSettings.h"
