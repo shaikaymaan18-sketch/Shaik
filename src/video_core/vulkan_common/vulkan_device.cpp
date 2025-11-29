db6b48c494bd11c1dcdb73897798a23174f08b16
Caio Oliveira <caiooliveirafarias0@gmail.com>
2025-11-28 22:38:14 -0300

    fixes build
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- src/video_core/vulkan_common/vulkan_device.cpp
+++ src/video_core/vulkan_common/vulkan_device.cpp
@@ -92,10 +92,12 @@ constexpr std::array VK_FORMAT_A4B4G4R4_UNORM_PACK16{
 
 } // namespace Alternatives
 
+#ifndef __APPLE__
 constexpr VkShaderStageFlags GraphicsStageMask =
     VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT |
     VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT | VK_SHADER_STAGE_GEOMETRY_BIT |
     VK_SHADER_STAGE_FRAGMENT_BIT;
+#endif
 
 template <typename T>
 void SetNext(void**& next, T& data) {
