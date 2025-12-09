d7603817390f3dc714faec385ce5d6391a5e7152
crueter <crueter@eden-emu.dev>
2025-12-09 01:09:31 -0500

    Fix license headers

--- src/Eden/Interface/MainWindowInterface.cpp
+++ src/Eden/Interface/MainWindowInterface.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "Eden/Models/GameListModel.h"
 #include "MainWindowInterface.h"
 #include "frontend_common/content_manager.h"
