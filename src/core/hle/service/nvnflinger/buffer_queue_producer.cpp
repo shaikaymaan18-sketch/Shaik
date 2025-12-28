d10e98c9710189b79b763b254c7c6489dd82fc5c
John <john@eden-emu.dev>
2025-12-28 08:26:00 +0100

    Update src/core/hle/service/nvnflinger/buffer_queue_producer.cpp

--- src/core/hle/service/nvnflinger/buffer_queue_producer.cpp
+++ src/core/hle/service/nvnflinger/buffer_queue_producer.cpp
@@ -941,7 +941,7 @@ void BufferQueueProducer::Transact(u32 code, std::span<const u8> parcel_data,
 
     const auto serialized = parcel_out.Serialize();
     std::memcpy(parcel_reply.data(), serialized.data(),
-                std::min(parcel_reply.size(), serialized.size()));
+                (std::min)(parcel_reply.size(), serialized.size()));
 }
 
 Kernel::KReadableEvent* BufferQueueProducer::GetNativeHandle(u32 type_id) {
