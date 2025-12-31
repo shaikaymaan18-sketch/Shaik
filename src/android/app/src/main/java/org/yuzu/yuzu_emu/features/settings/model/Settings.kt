1eb68300ec91144825a0fa99a1b3af97ef42eab2
kleidis <kleidis1@protonmail.com>
2025-12-31 01:27:24 +0100

    move paths setting entry up

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/Settings.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/Settings.kt
@@ -25,9 +25,9 @@ object Settings {
         SECTION_INPUT_PLAYER_SEVEN,
         SECTION_INPUT_PLAYER_EIGHT,
         SECTION_APP_SETTINGS(R.string.app_settings),
+        SECTION_CUSTOM_PATHS(R.string.preferences_custom_paths),
         SECTION_DEBUG(R.string.preferences_debug),
         SECTION_APPLETS(R.string.applets_menu);
-        SECTION_CUSTOM_PATHS(R.string.preferences_custom_paths);
     }
 
     fun getPlayerString(player: Int): String =
