79062d303e1c8f87b05e09f86f899de58f1d6539
chrelliott978 <chrelliott978@gmail.com>
2026-01-06 01:30:19 +0100

    Update src/shader_recompiler/backend/spirv/spirv_emit_context.cpp

--- src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
+++ src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
@@ -1695,10 +1695,11 @@ void EmitContext::DefineOutputs(const IR::Program& program) {
             frag_color[index] = DefineOutput(*this, output_type, std::nullopt);
             Decorate(frag_color[index], spv::Decoration::Location, index);
             Name(frag_color[index], fmt::format("frag_color{}", index));
-        }
+        } else {
             frag_color[index] = DefineOutput(*this, F32[4], std::nullopt);
             Decorate(frag_color[index], spv::Decoration::Location, index);
             Name(frag_color[index], fmt::format("frag_color{}", index));
+                        }
         }
         if (info.stores_frag_depth) {
             frag_depth = DefineOutput(*this, F32[1], std::nullopt);
