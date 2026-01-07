c62df6c76c6dc42bdf7c7ecf4ca0381676c0e79d
Maufeat <sahyno1996@gmail.com>
2026-01-07 03:44:18 +0100

    frick renderpass

--- src/video_core/renderer_vulkan/vk_query_cache.cpp
+++ src/video_core/renderer_vulkan/vk_query_cache.cpp
@@ -155,7 +155,7 @@ public:
         }
 
         ReserveHostQuery();
-
+/*
         // Ensure outside render pass
         scheduler.RequestOutsideRenderPassOperationContext();
 
@@ -167,7 +167,7 @@ public:
 
         // Manually restart the render pass (required for vkCmdClearAttachments, etc.)
         scheduler.RequestRenderpass(texture_cache.GetFramebuffer());
-
+*/
         // Begin query inside the newly started render pass
         scheduler.Record([query_pool = current_query_pool,
                                  query_index = current_bank_slot](vk::CommandBuffer cmdbuf) {
