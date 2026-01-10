0b654f1c8b5f6672e8d4a922668c3cf86ab0b679
Forrest Keller <forrestmarkx@outlook.com>
2026-01-10 07:50:21 -0600

    Removed uneeded header
    lowmemorydevice is now handled by a debug config

--- src/video_core/texture_cache/texture_cache.h
+++ src/video_core/texture_cache/texture_cache.h
@@ -70,15 +70,13 @@ TextureCache<P>::TextureCache(Runtime& runtime_, Tegra::MaxwellDeviceMemoryManag
             (std::max)((std::min)(device_local_memory - min_vacancy_critical, min_spacing_critical),
                      DEFAULT_CRITICAL_MEMORY));
         minimum_memory = static_cast<u64>((device_local_memory - mem_threshold) / 2);
-
-        lowmemorydevice = runtime.IsSteamDeck();
     } else {
         expected_memory = DEFAULT_EXPECTED_MEMORY + 512_MiB;
         critical_memory = DEFAULT_CRITICAL_MEMORY + 1_GiB;
         minimum_memory = 0;
-
-        lowmemorydevice = true;
     }
+
+    lowmemorydevice = Settings::values.enable_low_memory.GetValue();
 }
 
 template <class P>
