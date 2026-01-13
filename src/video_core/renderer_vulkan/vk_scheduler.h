7832f610ec34e49a1de030489aa9cae10fe852e6
DraVee <dravee@eden-emu.dev>
2026-01-13 15:54:12 +0100

    [vk] revert to previous value

--- src/video_core/renderer_vulkan/vk_scheduler.h
+++ src/video_core/renderer_vulkan/vk_scheduler.h
@@ -207,7 +207,7 @@ private:
 
         size_t command_offset = 0;
         bool submit = false;
-        alignas(std::max_align_t) std::array<u8, 0x40000> data{};
+        alignas(std::max_align_t) std::array<u8, 0x8000> data{};
     };
 
     struct State {
