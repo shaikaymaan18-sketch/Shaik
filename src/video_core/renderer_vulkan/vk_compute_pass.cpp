8464a8bcd0229b25cafe595998ee9cb037c23884
Nekle <224100951+ne-kle@users.noreply.github.com>
2026-01-25 04:30:45 +0100

    fx

--- src/video_core/renderer_vulkan/vk_compute_pass.cpp
+++ src/video_core/renderer_vulkan/vk_compute_pass.cpp
@@ -672,12 +672,6 @@ void ASTCDecoderPass::Assemble(Image& image, const StagingBufferRef& map,
                 .layerCount = VK_REMAINING_ARRAY_LAYERS,
             },
         };
-        // After ASTC decode, image may be used in shaders or as attachment
-        const VkPipelineStageFlags dst_stages_astc =
-            VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT |
-            VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT |
-            VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT |
-            VK_PIPELINE_STAGE_TRANSFER_BIT;
         cmdbuf.PipelineBarrier(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
                                VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0, image_barrier);
     });
