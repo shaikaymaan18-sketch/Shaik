55d93a4c342392be48af2b6ac120b1c1781e9b52
MaranBr <maranbr@outlook.com>
2026-02-11 16:12:41 -0400

    Fix build

--- src/video_core/renderer_vulkan/vk_scheduler.h
+++ src/video_core/renderer_vulkan/vk_scheduler.h
@@ -16,6 +16,7 @@
 
 #include "common/alignment.h"
 #include "common/common_types.h"
+#include "common/settings.h"
 #include "common/polyfill_thread.h"
 #include "video_core/renderer_vulkan/vk_master_semaphore.h"
 #include "video_core/vulkan_common/vulkan_wrapper.h"
