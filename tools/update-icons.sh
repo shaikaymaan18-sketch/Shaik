1313234e5e60d859246323ea1b9668ef70d56e6c
lizzie <lizzie@eden-emu.dev>
2026-02-23 18:44:19 +0000

    l

--- tools/update-icons.sh
+++ tools/update-icons.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-# SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
+# SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Updates main icons for eden
