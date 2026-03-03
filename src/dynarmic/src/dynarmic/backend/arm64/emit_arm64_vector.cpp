a816fe2a5772030af0a186125084b398f339e41c
crueter <crueter@eden-emu.dev>
2026-03-03 11:09:05 -0500

    Remove ifdefs
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- src/dynarmic/src/dynarmic/backend/arm64/emit_arm64_vector.cpp
+++ src/dynarmic/src/dynarmic/backend/arm64/emit_arm64_vector.cpp
@@ -866,17 +866,10 @@ void EmitIR<IR::Opcode::VectorMaxS32>(oaknut::CodeGenerator& code, EmitContext& 
 
 template<>
 void EmitIR<IR::Opcode::VectorMaxS64>(oaknut::CodeGenerator& code, EmitContext& ctx, IR::Inst* inst) {
-#ifdef __APPLE__
     EmitThreeOp(code, ctx, inst, [&](auto& Qresult, auto& Qa, auto& Qb) {
         code.CMGT(Qresult->D2(), Qa->D2(), Qb->D2());
         code.BSL(Qresult->B16(), Qa->B16(), Qb->B16());
     });
-#else
-    (void)code;
-    (void)ctx;
-    (void)inst;
-    UNREACHABLE();
-#endif
 }
 
 template<>
@@ -896,17 +889,10 @@ void EmitIR<IR::Opcode::VectorMaxU32>(oaknut::CodeGenerator& code, EmitContext& 
 
 template<>
 void EmitIR<IR::Opcode::VectorMaxU64>(oaknut::CodeGenerator& code, EmitContext& ctx, IR::Inst* inst) {
-#ifdef __APPLE__
     EmitThreeOp(code, ctx, inst, [&](auto& Qresult, auto& Qa, auto& Qb) {
         code.CMHI(Qresult->D2(), Qa->D2(), Qb->D2());
         code.BSL(Qresult->B16(), Qa->B16(), Qb->B16());
     });
-#else
-    (void)code;
-    (void)ctx;
-    (void)inst;
-    UNREACHABLE();
-#endif
 }
 
 template<>
@@ -926,17 +912,10 @@ void EmitIR<IR::Opcode::VectorMinS32>(oaknut::CodeGenerator& code, EmitContext& 
 
 template<>
 void EmitIR<IR::Opcode::VectorMinS64>(oaknut::CodeGenerator& code, EmitContext& ctx, IR::Inst* inst) {
-#ifdef __APPLE__
     EmitThreeOp(code, ctx, inst, [&](auto& Qresult, auto& Qa, auto& Qb) {
         code.CMGT(Qresult->D2(), Qb->D2(), Qa->D2());
         code.BSL(Qresult->B16(), Qa->B16(), Qb->B16());
     });
-#else
-    (void)code;
-    (void)ctx;
-    (void)inst;
-    UNREACHABLE();
-#endif
 }
 
 template<>
@@ -956,17 +935,10 @@ void EmitIR<IR::Opcode::VectorMinU32>(oaknut::CodeGenerator& code, EmitContext& 
 
 template<>
 void EmitIR<IR::Opcode::VectorMinU64>(oaknut::CodeGenerator& code, EmitContext& ctx, IR::Inst* inst) {
-#ifdef __APPLE__
     EmitThreeOp(code, ctx, inst, [&](auto& Qresult, auto& Qa, auto& Qb) {
         code.CMHI(Qresult->D2(), Qb->D2(), Qa->D2());
         code.BSL(Qresult->B16(), Qa->B16(), Qb->B16());
     });
-#else
-    (void)code;
-    (void)ctx;
-    (void)inst;
-    UNREACHABLE();
-#endif
 }
 
 template<>
