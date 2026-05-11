8b625508d8592d765d409099b98a1d142ba2839a
John <john@eden-emu.dev>
2026-05-11 19:23:05 +0200

    [common] Revert back Aync GPU default to true to fix flickering linux
    The cause of the flickering needs to be investigated but this will set it ON as default for desktop platforms.

--- src/common/settings.h
+++ src/common/settings.h
@@ -389,7 +389,7 @@ struct Values {
                                                   true};
 
     SwitchableSetting<bool> use_asynchronous_gpu_emulation{
-                                                           linkage, false, "use_asynchronous_gpu_emulation", Category::Renderer};
+                                                           linkage, true, "use_asynchronous_gpu_emulation", Category::Renderer};
     // *nix platforms may have issues with the borderless windowed fullscreen mode.
     // Default to exclusive fullscreen on these platforms for now.
     SwitchableSetting<FullscreenMode, true> fullscreen_mode{linkage,
