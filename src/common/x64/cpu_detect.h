3afbd6205f5ced001c426ab00af66301ce37308b
RedBlackAka <140876408+RedBlackAka@users.noreply.github.com>
2026-05-18 19:10:40 +0200

    Fix license header

--- src/common/x64/cpu_detect.h
+++ src/common/x64/cpu_detect.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // SPDX-FileCopyrightText: Copyright 2022 yuzu Emulator Project
 // SPDX-FileCopyrightText: Copyright 2013 Dolphin Emulator Project / 2015 Citra Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
