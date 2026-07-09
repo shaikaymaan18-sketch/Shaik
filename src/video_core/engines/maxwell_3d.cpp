6cc1edccb86c116f3de4223473871a39d4565f40
CamilleLaVey <camillelavey99@gmail.com>
2026-07-08 22:20:04 -0400

    Fix build

--- src/video_core/engines/maxwell_3d.cpp
+++ src/video_core/engines/maxwell_3d.cpp
@@ -223,7 +223,7 @@ void Maxwell3D::ProcessMacro(Core::System& system, u32 method, const u32* base_s
 }
 
 void Maxwell3D::RefreshParametersImpl() {
-    if (!settings::IsGPULevelHigh()) {
+    if (!Settings::IsGPULevelHigh()) {
         return;
     }
     size_t current_index = 0;
