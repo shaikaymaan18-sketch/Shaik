5a19647d8e67aa3170d507c8a6ab4a18827b2682
CamilleLaVey <camillelavey99@gmail.com>
2026-07-14 16:32:11 -0400

    [TEST] Removal on SignedNanPreserve for QCOM

--- src/shader_recompiler/backend/spirv/emit_spirv.cpp
+++ src/shader_recompiler/backend/spirv/emit_spirv.cpp
@@ -374,6 +374,9 @@ void SetupDenormControl(const Profile& profile, const IR::Program& program, Emit
 
 void SetupSignedNanCapabilities(const Profile& profile, const IR::Program& program,
                                 EmitContext& ctx, Id main_func) {
+    if (profile.has_broken_signed_nan_preserve) {
+        return;
+    }
     if (profile.has_broken_fp16_float_controls && program.info.uses_fp16) {
         return;
     }
