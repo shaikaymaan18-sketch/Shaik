527203956bc01bc6b598c7b87212277e83c23922
MaranBr <maranbr@outlook.com>
2026-07-15 21:28:31 -0400

    Improve GPU buffer readback to prevent visual bugs

--- src/video_core/buffer_cache/buffer_cache.h
+++ src/video_core/buffer_cache/buffer_cache.h
@@ -1688,8 +1688,10 @@ void BufferCache<P>::ImmediateUploadMemory([[maybe_unused]] Buffer& buffer,
                 if (immediate_buffer.empty()) {
                     immediate_buffer = ImmediateBuffer(largest_copy);
                 }
-                if (Settings::values.enable_gpu_buffer_readback.GetValue()) {
-                    DownloadBufferMemory(buffer, device_addr, copy.size);
+                if (IsRegionGpuModified(device_addr, copy.size) && Settings::values.enable_gpu_buffer_readback.GetValue()) {
+                    device_memory.ReadBlock(device_addr, immediate_buffer.data(), copy.size);
+                } else {
+                    device_memory.ReadBlockUnsafe(device_addr, immediate_buffer.data(), copy.size);
                 }
                 device_memory.ReadBlockUnsafe(device_addr, immediate_buffer.data(), copy.size);
                 upload_span = immediate_buffer.subspan(0, copy.size);
@@ -1709,8 +1711,10 @@ void BufferCache<P>::MappedUploadMemory([[maybe_unused]] Buffer& buffer,
         for (BufferCopy& copy : copies) {
             u8* const src_pointer = staging_pointer.data() + copy.src_offset;
             const DAddr device_addr = buffer.CpuAddr() + copy.dst_offset;
-            if (Settings::values.enable_gpu_buffer_readback.GetValue()) {
-                DownloadBufferMemory(buffer, device_addr, copy.size);
+            if (IsRegionGpuModified(device_addr, copy.size) && Settings::values.enable_gpu_buffer_readback.GetValue()) {
+                device_memory.ReadBlock(device_addr, src_pointer, copy.size);
+            } else {
+                device_memory.ReadBlockUnsafe(device_addr, src_pointer, copy.size);
             }
             device_memory.ReadBlockUnsafe(device_addr, src_pointer, copy.size);
             // Apply the staging offset
