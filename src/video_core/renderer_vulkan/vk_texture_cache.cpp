ef4a350060bf97bd19501898afed1b1e9ae90128
John <john@eden-emu.dev>
2026-07-18 19:22:46 +0200

    Update src/video_core/renderer_vulkan/vk_texture_cache.cpp

--- src/video_core/renderer_vulkan/vk_texture_cache.cpp
+++ src/video_core/renderer_vulkan/vk_texture_cache.cpp
@@ -145,8 +145,6 @@ constexpr VkBorderColor ConvertBorderColor(const std::array<float, 4>& color) {
 }
 
 [[nodiscard]] bool WillUseWidenedAstcFormat(const Device& device, const ImageInfo& info) {
-    // Disable widened ASTC formats to avoid VkFormat / GLSL image format mismatches
-    // that can cause Vulkan device loss. Always use A8B8G8R8_UNORM_PACK32 for ASTC decode.
     return false;
 }
 
