edc38b8230cd1911e499904a3149d5aae36e8796
xbzk <xbzk@eden-emu.dev>
2026-08-29 21:27:59 +0200

    Update src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/UShortSetting.kt
    removed acidental copy paste

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/UShortSetting.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/UShortSetting.kt
@@ -1,9 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-// SPDX-FileCopyrightText: 2023 yuzu Emulator Project
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 package org.yuzu.yuzu_emu.features.settings.model
 
 import org.yuzu.yuzu_emu.utils.NativeConfig
