9d975c587068f0e2c3a5ffdaed435def5683826e
PavelBARABANOV <pavelbarabanov94@gmail.com>
2026-09-01 04:16:13 +0300

    return SetLayerZIndex(layer_id, 100000)

--- src/core/hle/service/vi/shared_buffer_manager.cpp
+++ src/core/hle/service/vi/shared_buffer_manager.cpp
@@ -506,6 +506,8 @@ Result SharedBufferManager::PresentSharedFrameBuffer(android::Fence fence,
                  android::Status::NoError,
              VI::ResultOperationFailed);
 
+    (void)m_container.SetLayerZIndex(layer_id, 100000);
+
     // We succeeded.
     R_SUCCEED();
 }
