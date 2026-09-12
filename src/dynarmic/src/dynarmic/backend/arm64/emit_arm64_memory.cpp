38bc3533053e756bb21665a23a1ba921ce4fa1c7
PavelBARABANOV <pavelbarabanov94@gmail.com>
2026-09-12 06:13:12 +0300

    [dynarmic:] TST + B.NE for marked bit

--- src/dynarmic/src/dynarmic/backend/arm64/emit_arm64_memory.cpp
+++ src/dynarmic/src/dynarmic/backend/arm64/emit_arm64_memory.cpp
@@ -274,8 +274,8 @@ std::pair<oaknut::XReg, oaknut::XReg> InlinePageTableEmitVAddrLookup(oaknut::Cod
     code.LDR(Xscratch0, Xpagetable, Xscratch0);
 
     if (ctx.conf.page_table_marked_bit) {
-        code.UBFX(Xscratch1, Xscratch0, *ctx.conf.page_table_marked_bit, 1);
-        code.CBNZ(Xscratch1, *fallback);
+        code.TST(Xscratch0, 1ULL << *ctx.conf.page_table_marked_bit);
+        code.B(NE, *fallback);
     }
 
     if (ctx.conf.page_table_pointer_mask != 0) {
