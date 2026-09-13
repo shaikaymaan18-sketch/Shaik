83d5c53dc0c2708ec67339b5eb725fe1cd6eb577
lizzie <lizzie@eden-emu.dev>
2026-09-13 04:19:53 +0000

    2026-09-13 04:19:52
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/input_common/drivers/mouse.h
+++ src/input_common/drivers/mouse.h
@@ -116,7 +116,6 @@ private:
     Common::Vec<int, 2> wheel_position;
     Common::SteadyClock::time_point last_notify_timestamp{};
     bool button_pressed = false;
-    bool has_moved = false;
 };
 
 } // namespace InputCommon
