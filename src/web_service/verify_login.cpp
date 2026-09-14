8f014bafbccb60b13ead09ec00953acf4cda0bd3
crueter <crueter@eden-emu.dev>
2026-09-14 13:54:33 -0400

    Fix license headers

--- src/web_service/verify_login.cpp
+++ src/web_service/verify_login.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // SPDX-FileCopyrightText: 2017 Citra Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
